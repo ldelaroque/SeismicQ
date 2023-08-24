9cdc0fa840e12155fe5c1f0219e91f2224e382ca
Lorraine Delaroque <105422516+ldelaroque@users.noreply.github.com>
2023-08-24 15:38:42 +0200

    Update SeismicQ.jl
    Adding some comments.

--- src/SeismicQ.jl
+++ src/SeismicQ.jl
@@ -1,6 +1,14 @@
+#=
+File defining the "Seismic Q" module.
+=#
+
+# Module definition
 module SeismicQ
 
+# File executed in the module context
 include("Sources.jl")
+
+# Recovery of the Ricker function
 export Ricker
 
-end # module SeismicQ
+end 
